e87f05eb2755efead79201a5d9d84651e31a233c
Integralist <mark.mcdx@gmail.com>
2015-02-09 09:35:53 +0000

    Cruft created by the Leiningen project

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to spurious-aws-sdk-helper
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/spurious_aws_sdk_helper/core_test.clj
@@ -0,0 +1,7 @@
+(ns spurious-aws-sdk-helper.core-test
+  (:require [clojure.test :refer :all]
+            [spurious-aws-sdk-helper.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
